fbbb23d98b213e6ed0c8a790b525a7649631b103
you-5805 <you.iwamoto918@gmail.com>
2021-04-06 00:53:40 +0900

    deploy settings

--- src/App.js
+++ src/App.js
@@ -18,12 +18,25 @@ import { DraggableElement } from './components/Draggable';
 
 export const App = () => {
   return (
-    <div className="w-4/5 h-screen mx-auto mt-12 p-12 border text-center">
+    <div className="w-4/5 h-96 mx-auto mt-12 p-12 border text-center">
       <h1 className="text-3xl font-bold mb-8">drag and drop</h1>
       <div className="container w-12 h-40 mx-auto">
         <DraggableElement color="red" />
         <DraggableElement color="green" />
         <DraggableElement color="yellow" />
+      </div>
+      <div className="h-12 w-96 ml-auto text-right">
+        <p>by @you-5805</p>
+        <p>
+          <a
+            href="https://github.com/you-5805/drag-and-drop-DOM-elements"
+            className="text-blue-400 hover:text-blue-600 underline whitespace-nowrap text-sm"
+            rel="noreferrer"
+            target="_blank"
+          >
+            https://github.com/you-5805/drag-and-drop-DOM-elements
+          </a>
+        </p>
       </div>
     </div>
   );
